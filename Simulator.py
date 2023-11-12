5afaea01bbb687a9f65ce074a69222cdab5d4d5a
Mattc2021 <mattc2017@gmail.com>
2023-11-12 15:36:25 -0600

    Updating code a bit

--- Simulator.py
+++ Simulator.py
@@ -5,27 +5,30 @@ import csv
 from tqdm import tqdm
 
 class ChessSimulation:
-    def __init__(self, num_games, player1, player2):
+    def __init__(self, num_games, player1, player2, stockfish_path):
         self.num_games = num_games
         self.player1 = player1
-        self.engine = chess.engine.SimpleEngine.popen_uci("./stockfish-windows-x86-64-avx2.exe")
+        self.player2 = player2
+        self.stockfish_path = stockfish_path
         self.results = []
 
     def run_simulation(self):
-        for _ in tqdm(range(self.num_games), desc="Simulating Games", unit="game"):
-            self.play_game()
+        self.engine = chess.engine.SimpleEngine.popen_uci(self.stockfish_path)
+        for game_num in tqdm(range(self.num_games), desc="Simulating Games", unit="game"):
+            white, black = (self.player1, self.player2) if game_num < self.num_games / 2 else (self.player2, self.player1)
+            self.play_game(white, black)
+        self.engine.quit()
 
-    def play_game(self):
+    def play_game(self, white, black):
         board = chess.Board()
         moves = []
 
         while not board.is_game_over():
             if board.turn == chess.WHITE:
-                move = self.player1.choose_move(board)
+                move = self.get_move(white, board)
             else:
-                result = self.engine.play(board, chess.engine.Limit(time=0.1))  # Stockfish move with a time limit
-                move = result.move
-            
+                move = self.get_move(black, board)
+
             board.push(move)
             moves.append(str(move))
 
@@ -33,10 +36,17 @@ class ChessSimulation:
         self.results.append({
             "winner": self.get_winner(result),
             "loser": self.get_loser(result),
-            "white_pieces": str(self.player1),
-            "black_pieces": "Stockfish",
+            "white_pieces": str(white),
+            "black_pieces": str(black),
             "moves": ', '.join(moves)
         })
+
+    def get_move(self, player, board):
+        if isinstance(player, AlphaPawn):
+            return player.choose_move(board)
+        else:
+            result = self.engine.play(board, chess.engine.Limit(time=0.1))
+            return result.move
 
     def get_winner(self, result):
         if result == '1-0':
@@ -64,15 +74,13 @@ class ChessSimulation:
     def close_engine(self):
         self.engine.quit()
 
-# Example usage
 if __name__ == "__main__":
-    num_simulated_games = 100
+    num_simulated_games = 10
     ai_player = AlphaPawn()  # Your AI
-    stockfish_path = "/stockfish-windows-x86-64-avx2.exe"  # Replace with your Stockfish path
+    stockfish_path = "./stockfish-windows-x86-64-avx2.exe"  # Replace with your Stockfish path
 
-    simulation = ChessSimulation(num_simulated_games, ai_player, stockfish_path)
+    simulation = ChessSimulation(num_simulated_games, ai_player, "Stockfish", stockfish_path)
     simulation.run_simulation()
     simulation.write_to_csv("chess_simulation_results.csv")
-    simulation.close_engine()
 
     print("Simulation complete. Results saved to chess_simulation_results.csv")
