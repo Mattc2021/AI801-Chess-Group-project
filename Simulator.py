f16ef0e05d17096ffd8a4f541df70fb49523c11a
mattc_2021 <mattc2017@gmail.com>
2023-11-16 00:37:23 -0600

    Creating simulator to output files as tensors to train CNN

--- Simulator.py
+++ Simulator.py
@@ -1,8 +1,31 @@
 from AlphaPawn import AlphaPawn
-import chess.svg
+import chess
+import numpy as np
+import torch
 import chess.engine
 import csv
 from tqdm import tqdm
+from datetime import date
+
+def board_to_matrix(board):
+    piece_to_value = {
+        chess.PAWN: 1, chess.KNIGHT: 2, chess.BISHOP: 3, chess.ROOK: 4, chess.QUEEN: 5, chess.KING: 6
+    }
+    matrix = np.zeros((8, 8), dtype=int)
+    
+    for square in chess.SQUARES:
+        piece = board.piece_at(square)
+        if piece:
+            value = piece_to_value[piece.piece_type]
+            if piece.color == chess.BLACK:
+                value = -value
+            matrix[chess.square_rank(square), chess.square_file(square)] = value
+
+    return matrix
+
+def board_to_tensor(board):
+    matrix = board_to_matrix(board)
+    return torch.tensor(matrix, dtype=torch.float32)
 
 class ChessSimulation:
     def __init__(self, num_games, player1, player2, stockfish_path):
@@ -15,12 +38,17 @@ class ChessSimulation:
     def run_simulation(self):
         self.engine = chess.engine.SimpleEngine.popen_uci(self.stockfish_path)
         for game_num in tqdm(range(self.num_games), desc="Simulating Games", unit="game"):
-            white, black = (self.player1, self.player2) if game_num < self.num_games / 2 else (self.player2, self.player1)
-            self.play_game(white, black)
+            white, black = (self.player1, self.player2) if game_num % 2 == 0 else (self.player2, self.player1)
+            tensor_states, evaluations = self.play_game(white, black)
+            self.save_tensors(tensor_states, game_num, evaluations)
+
         self.engine.quit()
+        self.write_to_csv("chess_simulation_results.csv")
 
     def play_game(self, white, black):
         board = chess.Board()
+        tensor_states = []
+        evaluations = []  # List to store Stockfish evaluations
         moves = []
 
         while not board.is_game_over():
@@ -30,22 +58,31 @@ class ChessSimulation:
                 move = self.get_move(black, board)
 
             board.push(move)
+            tensor_states.append(board_to_tensor(board))
             moves.append(str(move))
 
-        result = board.result()
-        self.results.append({
-            "winner": self.get_winner(result),
-            "loser": self.get_loser(result),
+            # Get Stockfish evaluation for the current board state
+            evaluation = self.engine.analyse(board, chess.engine.Limit(time=0.1))
+            score = evaluation["score"].white().score(mate_score=10000)  # Convert to a consistent perspective
+            evaluations.append(score)
+
+        game_result = {
+            "winner": self.get_winner(board.result()),
+            "loser": self.get_loser(board.result()),
             "white_pieces": str(white),
             "black_pieces": str(black),
-            "moves": ', '.join(moves)
-        })
+            "moves": ', '.join(moves),
+            "evaluations": evaluations
+        }
+        self.results.append(game_result)
+
+        return tensor_states, evaluations
 
     def get_move(self, player, board):
         if isinstance(player, AlphaPawn):
             return player.choose_move(board)
         else:
-            result = self.engine.play(board, chess.engine.Limit(time=0.1))
+            result = self.engine.play(board, chess.engine.Limit(time=0.1))  # Reduced time for faster simulation
             return result.move
 
     def get_winner(self, result):
@@ -64,23 +101,24 @@ class ChessSimulation:
         else:
             return 'Draw'
 
+    def save_tensors(self, tensor_states, game_num, evaluations):
+        file_name = f"./chess_position_tensors/chess_game_{game_num}_states_{str(date.today())}.pt"
+        torch.save({"states": tensor_states, "evaluations": evaluations}, file_name)
+        print(f"Saved {len(tensor_states)} tensors and evaluations to {file_name}")
+
     def write_to_csv(self, file_name):
         with open(file_name, mode='w', newline='', encoding='utf-8') as file:
-            writer = csv.DictWriter(file, fieldnames=["winner", "loser", "white_pieces", "black_pieces", "moves"])
+            writer = csv.DictWriter(file, fieldnames=["winner", "loser", "white_pieces", "black_pieces", "moves", "evaluations"])
             writer.writeheader()
             for game_result in self.results:
                 writer.writerow(game_result)
 
-    def close_engine(self):
-        self.engine.quit()
-
 if __name__ == "__main__":
-    num_simulated_games = 10
+    num_simulated_games = 20  # Adjust the number of games as needed
     ai_player = AlphaPawn()  # Your AI
     stockfish_path = "./stockfish-windows-x86-64-avx2.exe"  # Replace with your Stockfish path
 
-    simulation = ChessSimulation(num_simulated_games, ai_player, "Stockfish", stockfish_path)
+    simulation = ChessSimulation(num_simulated_games, "Stockfish", "Stockfish", stockfish_path)
     simulation.run_simulation()
-    simulation.write_to_csv("chess_simulation_results.csv")
 
-    print("Simulation complete. Results saved to chess_simulation_results.csv")
+    print("Simulation complete. Tensors and game results saved.")
---
+++ load_tensors.py
@@ -0,0 +1,19 @@
+import torch
+
+def load_tensors(file_name):
+    data = torch.load(file_name)
+    tensor_states = data['states']
+    evaluations = data['evaluations']
+    return tensor_states, evaluations
+
+def display_tensor_info(tensor_states, evaluations):
+    print(f"Total number of board states: {len(tensor_states)}")
+    for i, (tensor, eval) in enumerate(zip(tensor_states, evaluations)):
+        print(f"\nBoard State {i+1}:")
+        print(tensor.numpy())  # Convert tensor to numpy array for easy viewing
+        print(f"Stockfish Evaluation: {eval}")
+
+if __name__ == "__main__":
+    file_name = './chess_position_tensors/chess_game_0_states_2023-11-16.pt'  # Replace with your actual file name
+    tensor_states, evaluations = load_tensors(file_name)
+    display_tensor_info(tensor_states, evaluations)
