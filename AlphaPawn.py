3ae3441baaaebf32c4d95541813914105a74fe36
TJ Gallagher <51892545+tvg5197@users.noreply.github.com>
2023-11-15 19:06:53 -0500

    Added: CNN model now autosaves to C:\StrategosAI

--- AlphaPawn.py
+++ AlphaPawn.py
@@ -1,4 +1,6 @@
 from copy import copy, deepcopy
+from imp import load_module
+import os
 import AlphaPawn
 import chess.svg
 import tkinter as tk
@@ -7,6 +9,7 @@ import random
 import numpy as np
 import threading
 import time
+from keras import Model as KerasModel
 
 PIECE_VALUES = {
     chess.PAWN: 1,
@@ -18,12 +21,21 @@ PIECE_VALUES = {
     
 }
 
-import tensorflow.python.keras as tfk
 import tensorflow as tf
 
+import os
+import threading
+import time
+from tensorflow import keras as tfk
+
+from keras.models import load_model
 class CNNChessModel:
     def __init__(self):
-        self.model = self.build_cnn()
+        self.model = self.load_or_build_cnn()
+        self.SAVE_PATH = "C:\\StrategosAI"  # Define the save path
+        self.MODEL_NAME = 'saved_model'
+       
+        threading.Thread(target=self.CNN_autosave, daemon=True).start()
 
     def build_cnn(self):
         # Build and compile the CNN model using TensorFlow
@@ -35,7 +47,38 @@ class CNNChessModel:
         ])
         model.compile(optimizer='adam', loss='categorical_crossentropy')
         return model
-
+    
+    def load_or_build_cnn(self):
+        # Check if the directory and model file exist
+        self.SAVE_PATH = "C:\\StrategosAI"
+        self.MODEL_NAME = 'saved_model'
+        if os.path.exists(os.path.join(self.SAVE_PATH, self.MODEL_NAME)):
+            # Load the model if the file exists
+            try:
+                loaded_model = load_model(os.path.join(self.SAVE_PATH, self.MODEL_NAME))
+                print("Model loaded successfully.")
+                return loaded_model
+            except Exception as e:
+                print("Error loading the model:", e)
+                print("Building a new model...")
+                return self.build_cnn()
+        else:
+            print(f"No model found in {self.SAVE_PATH}. Building a new model...")
+            return self.build_cnn()
+        
+    def CNN_autosave(self):
+        while True:
+            # Save the model if it's available and is a Keras model
+            if isinstance(self.model, KerasModel):
+                try:
+                    self.model.save(os.path.join(self.SAVE_PATH, self.MODEL_NAME))
+                    print("Model saved successfully.")
+                except Exception as e:
+                    print("Error saving model:", e)
+            else:
+                print("No Keras model available to save.")
+                
+            time.sleep(15)  # Wait for 15 seconds
 
 # Define MCTS algorithm
 class MCTS:
@@ -153,6 +196,7 @@ class ChessGUI:
         self.piece_images = piece_images
         print("starting thread...")
         self.choose_side()
+   
         threading.Thread(target=self.print_turn_periodically, daemon=True).start()
         # Attributes for game state
 
@@ -229,6 +273,8 @@ class ChessGUI:
                 print("Player's color: White")
             print("---")
             time.sleep(5)  # Wait for 5 seconds
+            
+
             
     def start_game(self, player_color):
 
@@ -399,6 +445,7 @@ class ChessGUI:
 
 
 if __name__ == "__main__":
+   
     root = tk.Tk()
     root.title("Strategos Chess")
 
